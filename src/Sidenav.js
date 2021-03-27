dc530a3199fbe12703eb3163ac8228620a0645f3
Kevin Laborda <klaborda@gmail.com>
2021-03-27 09:47:47 -0600

    init layout

---
+++ src/Sidenav.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+
+function Sidenav() {
+  return (
+    <div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Home</li>
+        <li class="list-group-item">About Me</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidenav;
