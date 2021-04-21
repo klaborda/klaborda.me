7ba79f2d3c63d9f7d2f6e0aec55f085928fb5e00
Kevin Laborda <klaborda@gmail.com>
2021-04-20 20:04:28 -0600

    init render of posts including markdown support

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,38 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+const gfm = require("remark-gfm");
 
 function BlogList() {
-  return <div>List of Blog Posts</div>;
+  const posts = [
+    {
+      id: "3",
+      createdAt: "2021-04-20T05:01:54.944Z",
+      title: "Third Post",
+      body: "# Third Post\n\nWeeee",
+      author: {
+        name: "Kevin",
+      },
+    },
+  ];
+
+  return (
+    <div>
+      {posts &&
+        posts.map((post) => {
+          return (
+            <>
+              <small key={post?.id}>Thoughts from {post?.author?.name}</small>
+              <div>
+                <ReactMarkdown
+                  plugins={[gfm]}
+                  children={post?.body}
+                ></ReactMarkdown>
+              </div>
+            </>
+          );
+        })}
+    </div>
+  );
 }
 
 export default BlogList;
