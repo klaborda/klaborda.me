3f221c87a3c698447f184ee8491125b9c3e72f01
Kevin Laborda <klaborda@gmail.com>
2021-04-20 20:44:45 -0600

    connect to graphql query

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import Moment from "react-moment";
+import { useQuery, gql } from "@apollo/client";
 const gfm = require("remark-gfm");
+
+const FEED_QUERY = gql`
+  {
+    feed(orderBy: { createdAt: desc }) {
+      id
+      createdAt
+      title
+      body
+      author {
+        name
+      }
+    }
+  }
+`;
 
 function BlogList() {
   const posts = [
@@ -16,14 +31,16 @@ function BlogList() {
     },
   ];
 
+  const { data } = useQuery(FEED_QUERY);
+
   return (
     <div>
-      {posts &&
-        posts.map((post) => {
+      {data &&
+        data.feed.map((post) => {
           return (
             <>
               <small key={post?.id}>
-                Thoughts from {post?.author?.name} on{" "}
+                Thoughts from {post?.author?.name || "Unknown"} on{" "}
                 <Moment format="YYYY/MM/DD">{post?.createdAt}</Moment>
               </small>
               <div>
